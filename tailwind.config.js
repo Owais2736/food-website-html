a51bed9260377cdd041b879260c535dfa119a0ee
owais ali <owaisali72359@gmail.com>
2024-10-30 17:07:33 +0500

    food web

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1950px', 
+      },
+    },
+  },
+  plugins: [],
+}
